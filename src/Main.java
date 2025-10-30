bfc70d6a7b98a0c7c48043836e5fe6d9102a8499
M <m@MacBook-Air-mjed.local>
2025-10-30 13:56:02 +0300

    another update

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,52 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        
-        while (true){
+        ArrayList<Shape>search = new ArrayList<>();
+
+            try {
+        boolean t = true;
+        while (t){
             System.out.println();
-            System.out.println("--------------- to calculate area and circumference  ------------------\nif you want Circle press 1 :\nif you want tringle press 2 :\nif you want rectangle press 3 :");
+            System.out.println("--------------- to calculate area and circumference ---------------\n1-if you want Circle press 1 :\n2-if you want Tringle press 2 :\n3-if you want Rectangle press 3 :\n* if you want Exit Enter any number Large than 3 *");
+            System.out.print("- Enter here Please -> :");
             int userChoice = input.nextInt();
             switch (userChoice){
                 case 1:
-                    System.out.println("Enter a radius:");
+                    System.out.println();
+                    System.out.println("- Enter a radius:");
                     double choice = input.nextDouble();
-                    Circle circle2 = new Circle(choice);
-                    System.out.println(circle2);
+                    Circle circle = new Circle(choice);
+                    System.out.println(circle);
                     break;
                 case 2:
-                    System.out.println("Enter a width:");
+                    System.out.println("- Enter a width:");
                     double width = input.nextDouble();
-                    System.out.println("Enter a base:");
+                    System.out.println("- Enter a base:");
                     double base = input.nextDouble();
                     Triangle triangle1 = new Triangle(width , base);
                     System.out.println(triangle1);
                     break;
                 case 3:
-                    System.out.println("Enter a width:");
-                    double width1 = input.nextDouble();
-                    System.out.println("Enter a base:");
+                    System.out.println("- Enter a width:");
+                    width = input.nextDouble();
+                    System.out.println("- Enter a base:");
                     double height = input.nextDouble();
-                    Rectangle rectangle1 = new Rectangle(width1 , height);
+                    Rectangle rectangle1 = new Rectangle(width , height);
                     System.out.println(rectangle1);
                     break;
                 default:
+                    t = false;
                     break;
 
             }
         }
+            }catch (InputMismatchException e){
+                System.out.println("Please enter just numbers !");
+            }
             }
         }
 
